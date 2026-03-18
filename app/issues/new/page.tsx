0b8032538aa8b9ebbf489c44d98b7427708bd292
PShinde630 <pshinde2@csuchico.edu>
2026-03-18 14:50:19 -0700

    added markdown editor for issue description

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React from 'react'
 import { TextArea, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 
 const NewIssuePage = () => {
@@ -12,7 +14,7 @@ const NewIssuePage = () => {
           className="w-full rounded-md border border-zinc-300 px-3 py-2 outline-none focus:border-zinc-500"
         />
 
-        <TextArea placeholder="Description:" />
+        <SimpleMDE placeholder="Description:" />
 
         <Button>Submit New Issue</Button>
 
